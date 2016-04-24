8d592d7d118a3d5c1a45ad33adfa30f7f5db5598
durisova65 <natalyduris@gmail.com>
2016-04-24 22:19:50 +0200

    Update users.php

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -14,42 +14,17 @@ class Users extends CI_Controller {
     }
 
     function index() {
-
         $this->load->view('users_view');
     }
-
-    function addNewEvent() {
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('name', 'Event name', 'trim|required');
-        $this->form_validation->set_rules('description', 'Description', 'trim|required');
-        $this->form_validation->set_rules('date', 'Date', 'trim|required');
-
-        // ak prejde validaciou a uspesne sa zaregistruje
-        if ($this->form_validation->run()) {
-            if ($this->events_model->addNewEvent()) {
-                redirect('events');
-            } else {
-                echo 'add event denied.';
-            }
-        }
-
-        $this->load->view('addNewEvent_view');
-    }
     
-    function displayEvent($id){
-        $data = $this->events_model->getEventData($id);
-        $this->load->view('event_view', $data);
-        
-    }
     
-    function deleteEvent($id){
-        if($this->events_model->deleteEvent($id)){
-            redirect('events');
+    function deleteUser($id){
+        if($this->events_model->deleteUser($id)){
+            redirect('users');
         }else {
             echo 'delete event denied.';
         }
-        
-        $this->load->view('event_view', $this->events_model->getEventData($id));
+        $this->load->view('users_view');
     }
 
 }
